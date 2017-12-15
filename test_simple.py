f71420ae7a6cfcf2754a3e207deacdbf1b9ffa6f
Eric @AusTech teamviewer <eric.yeh@pioneerm.com>
2017-12-15 13:13:31 +1100

    australia changes

--- test_simple.py
+++ test_simple.py
@@ -1,6 +1,10 @@
 from pymodbus.client.sync import ModbusTcpClient
+import logging
 
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
-client = ModbusTcpClient('192.168.0.52', port=502)
-print(client.read_holding_registers(address=1, count=124, unit=1).registers)
+client = ModbusTcpClient('192.168.11.51', port=502)
+print(client.read_holding_registers(address=256, count=380, unit=1).registers)
 client.close()
